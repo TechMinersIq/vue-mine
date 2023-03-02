73194354423a792935731a232421340549d8d2ee
Zaid Kindman <zaid1997uk@gmail.com>
2023-03-02 19:34:38 +0300

    add custom themes for vuetify

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,8 +1,46 @@
-// Styles
 import "@mdi/font/css/materialdesignicons.css";
 import "vuetify/styles";
 
-// Vuetify
-import { createVuetify } from "vuetify";
+import { createVuetify, type ThemeDefinition } from "vuetify";
 
-export default createVuetify();
+const customLightTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    background: "#FFFFFF",
+    surface: "#FFFFFF",
+    primary: "#6200EE",
+    "primary-darken-1": "#3700B3",
+    secondary: "#03DAC6",
+    "secondary-darken-1": "#018786",
+    error: "#B00020",
+    info: "#2196F3",
+    success: "#4CAF50",
+    warning: "#FB8C00",
+  },
+};
+
+const customDarkTheme: ThemeDefinition = {
+  dark: true,
+  colors: {
+    background: "#424242",
+    surface: "#424242",
+    primary: "#6200EE",
+    "primary-darken-1": "#3700B3",
+    secondary: "#03DAC6",
+    "secondary-darken-1": "#018786",
+    error: "#B00020",
+    info: "#2196F3",
+    success: "#4CAF50",
+    warning: "#FB8C00",
+  },
+};
+
+export default createVuetify({
+  theme: {
+    defaultTheme: "customLightTheme",
+    themes: {
+      customLightTheme,
+      customDarkTheme,
+    },
+  },
+});
