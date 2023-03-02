c28b8c9a28a4a88b08001da1772801fde27ba9a1
Zaid Kindman <zaid1997uk@gmail.com>
2023-03-02 10:24:41 +0300

    use .env file to store meta information

--- src/plugins/request.ts
+++ src/plugins/request.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://jsonplaceholder.typicode.com/",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-type": "application/json",
   },
