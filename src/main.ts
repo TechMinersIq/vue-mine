ef3b071b179abffb63318c5345be365e2f6497db
Zaid Kindman <zaid1997uk@gmail.com>
2023-03-02 13:21:44 +0300

    remove vuetify

--- src/main.ts
+++ src/main.ts
@@ -3,23 +3,10 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/main.css";
-import "vuetify/styles";
-import { createVuetify } from "vuetify";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
-
-const vuetify = createVuetify({
-  components,
-  directives,
-  theme: {
-    defaultTheme: "dark",
-  },
-});
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-app.use(vuetify);
 
 app.mount("#app");
