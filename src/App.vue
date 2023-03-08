c0782a32202ed181c7bd5d1d5d7fe22fe98a97a2
Zaid Kindman <zaid1997uk@gmail.com>
2023-03-08 21:08:29 +0300

    add quasar to project

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,14 @@
 <template>
-  <router-view />
+  <q-layout>
+    <q-header>
+      <q-toolbar>
+        {{ $t("systemMeta.title") }}
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <q-page>
+        <router-view />
+      </q-page>
+    </q-page-container>
+  </q-layout>
 </template>
