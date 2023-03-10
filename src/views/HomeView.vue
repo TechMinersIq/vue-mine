3043e8afa1b3a90e99c62e48db1a021501aa8a68
Zaid Kindman <38330943+ZaidKindman@users.noreply.github.com>
2023-03-10 10:51:24 +0300

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,12 +7,3 @@
     </v-card>
   </v-container>
 </template>
-<script setup lang="ts">
-import { useLocale } from "vuetify";
-
-const { current } = useLocale();
-
-const changeLocale = (locale: string) => {
-  current.value = locale;
-};
-</script>
