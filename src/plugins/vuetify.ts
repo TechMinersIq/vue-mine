d08d3cfd0398074987a49297d7886afa63556cdf
Zaid Kindman <38330943+ZaidKindman@users.noreply.github.com>
2023-03-10 13:29:36 +0300

    add custom vuetify theme

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -9,7 +9,29 @@ import i18n from "@/plugins/i18n";
 import { useI18n } from "vue-i18n";
 import { createVueI18nAdapter } from "vuetify/locale/adapters/vue-i18n";
 
+const myCustomLightTheme = {
+  dark: false,
+  colors: {
+    background: "#FFFFFF",
+    surface: "#FFFFFF",
+    primary: "#6200EE",
+    "primary-darken-1": "#3700B3",
+    secondary: "#03DAC6",
+    "secondary-darken-1": "#018786",
+    error: "#B00020",
+    info: "#2196F3",
+    success: "#4CAF50",
+    warning: "#FB8C00",
+  },
+};
+
 export default createVuetify({
+  theme: {
+    defaultTheme: "light",
+    themes: {
+      myCustomLightTheme,
+    },
+  },
   locale: {
     adapter: createVueI18nAdapter({ i18n, useI18n }),
   },
