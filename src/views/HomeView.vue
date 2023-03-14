1329ba9e9ddbf73e36e882e529a9ab26a0e031ff
Zaid Kindman <zaid1997uk@gmail.com>
2023-03-14 23:09:45 +0300

    change wireframe

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <template>
-  <v-container>
+  <v-container fluid>
     <v-card flat>
       <v-toolbar color="primary">
-        <h1>{{ $t("system.title") }}</h1>
+        <h1 class="pa-5">{{ $t("system.title") }}</h1>
       </v-toolbar>
     </v-card>
   </v-container>
