bb3a28af675d82e2a4ebd3d63c8efde29a2a3c6f
Zaid Kindman <zaid1997uk@gmail.com>
2023-03-18 22:30:29 +0300

    add language selector component

---
+++ src/components/LanguageSelector.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-select
+    :label="$t('appBar.language')"
+    density="compact"
+    v-model="locale"
+    class="mt-5 mx-2"
+    @update:modelValue="changeLocale"
+    :items="['ar', 'en']"
+  ></v-select>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+import { useLocale } from "vuetify";
+
+const { current } = useLocale();
+
+const locale = ref("en");
+const changeLocale = (value: string) => {
+  current.value = value;
+};
+</script>
--- src/layouts/AppBar.vue
+++ src/layouts/AppBar.vue
@@ -3,12 +3,14 @@
     <!-- <v-app-bar-nav-icon @click="props.toggleDrawer" /> -->
     <v-toolbar-title>{{ $t("system.title") }}</v-toolbar-title>
     <v-spacer />
+    <LanguageSelector />
     <ThemeSwitcher />
   </v-app-bar>
 </template>
 
 <script setup lang="ts">
 import ThemeSwitcher from "@/components/ThemeSwitcher.vue";
+import LanguageSelector from "@/components/LanguageSelector.vue";
 const props = defineProps({
   toggleDrawer: {
     type: Function,
