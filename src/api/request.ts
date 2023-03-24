80fc580b50e8cc3cc89cee02a1e79024eb2d7338
Zaid Kindman <zaid1997uk@gmail.com>
2023-03-24 12:37:00 +0300

    Update request.ts

--- src/api/request.ts
+++ src/api/request.ts
@@ -8,13 +8,6 @@ request.defaults.headers.common["Content-Type"] = "application/json";
 
 request.interceptors.request.use(
   (config) => {
-    /* 
-    use pinia to store overlay value or loader
-    */
-
-    /**
-     * set bearer token
-     */
     return config;
   },
   (error) => Promise.reject(error)
@@ -22,9 +15,6 @@ request.interceptors.request.use(
 
 request.interceptors.response.use(
   function (response) {
-    /*
-    use pinia to store overlay value or loader
-    */
     return response;
   },
   function (error) {
@@ -39,9 +29,6 @@ request.interceptors.response.use(
         console.log("Server Error");
         break;
       default:
-        /*
-          route to 404 page or unauthorized page ...etc
-        */
         break;
     }
     return Promise.reject(error);
