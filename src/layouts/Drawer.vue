f40ff35f74c10c9daa5c6f903832c271af927eeb
Zaid Kindman <zaid1997uk@gmail.com>
2023-08-13 12:06:46 +0300

    Update Drawer.vue

--- src/layouts/Drawer.vue
+++ src/layouts/Drawer.vue
@@ -1,52 +1,44 @@
 <template>
-  <v-navigation-drawer theme="dark" rail permanent>
-    <v-list-item
-      nav
-      prepend-avatar="https://avatars.githubusercontent.com/u/124864700?s=200&v=4"
-    ></v-list-item>
+  <v-navigation-drawer
+    color="indigo-darken-4"
+    theme="dark"
+    permanent
+    :rail="!$vuetify.display.mdAndUp"
+  >
+    <template v-slot:prepend>
+      <v-list-item
+        class="pa-2"
+        lines="two"
+        prepend-avatar="https://avatars.githubusercontent.com/u/38330943?v=4"
+        title="Zaid Kindman"
+        subtitle="Welcome back"
+      ></v-list-item>
+    </template>
 
     <v-divider></v-divider>
 
     <v-list density="compact" nav>
       <v-list-item
-        prepend-icon="mdi-view-dashboard"
-        value="dashboard"
-      ></v-list-item>
-
-      <v-list-item prepend-icon="mdi-forum" value="messages"></v-list-item>
-    </v-list>
-    <template v-slot:append>
-      <div class="pa-2">
-        <v-btn variant="plain" icon
-          ><v-icon color="red">mdi-logout</v-icon>
-        </v-btn>
-      </div>
-    </template>
-  </v-navigation-drawer>
-
-  <!-- <v-navigation-drawer
-    class="bg-deep-purple"
-    theme="dark"
-    v-model="props.drawer"
-  >
-    <v-list density="compact" nav>
-      <v-list-item
         prepend-icon="mdi-home-city"
-        title="Home"
+        :title="$t('drawer.home')"
+        to="/"
         value="home"
       ></v-list-item>
       <v-list-item
         prepend-icon="mdi-account"
-        title="My Account"
-        value="account"
-      ></v-list-item>
-      <v-list-item
-        prepend-icon="mdi-account-group-outline"
-        title="Users"
-        value="users"
+        :title="$t('drawer.about')"
+        to="/about"
+        value="about"
       ></v-list-item>
     </v-list>
-  </v-navigation-drawer> -->
+    <template v-slot:append>
+      <div class="pa-2">
+        <v-btn color="red-darken-4" block>
+          <v-icon>mdi-logout</v-icon>
+        </v-btn>
+      </div>
+    </template>
+  </v-navigation-drawer>
 </template>
 <script setup lang="ts">
 const props = defineProps({
